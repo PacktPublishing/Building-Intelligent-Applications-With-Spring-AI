4c0b6a0c262f5288c30208fdc22824ae24cbe9f7
John Blum <jblum0@gmail.com>
2024-10-02 23:27:50 -0700

    Add example Spring AI application demonstrating Audio Transcription.

---
+++ audio-transcription-example/src/main/java/com/packt/spring/ai/examples/audio/transcription/AudioTranscriptionApplication.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.packt.spring.ai.examples.audio.transcription;
+
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * {@link SpringBootApplication} using Spring AI with OpenAI and the Whisper model to demonstrate Audio Transcription.
+ *
+ * @author John Blum
+ * @see org.springframework.ai.openai.OpenAiAudioTranscriptionModel
+ * @see org.springframework.boot.ApplicationRunner
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.boot.builder.SpringApplicationBuilder
+ * @since 0.1.0
+ */
+@SpringBootApplication
+@SuppressWarnings("unused")
+public class AudioTranscriptionApplication {
+
+	private static final String USER_PROFILE = "user";
+
+	public static void main(String[] args) {
+
+		new SpringApplicationBuilder(AudioTranscriptionApplication.class)
+			.web(WebApplicationType.NONE)
+			.profiles(USER_PROFILE)
+			.build()
+			.run(args);
+	}
+
+	@Bean
+	ApplicationRunner programRunner(OpenAiAudioTranscriptionModel transcriptionModel) {
+
+		return args -> {
+
+			Resource audio = new ClassPathResource("audio.mp3");
+
+			String text = transcriptionModel.call(audio);
+
+			System.out.printf("\"%s\"%n", text.trim());
+		};
+	}
+}
