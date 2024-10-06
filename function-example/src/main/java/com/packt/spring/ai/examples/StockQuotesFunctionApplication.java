f17b560af06d7f5ce2ddfa4e4f1f3afbb5dd5e50
John Blum <jblum0@gmail.com>
2024-10-05 20:45:35 -0700

    Enhance StockQuotesFunctionApplication with Spring AI Function Calling support using Ollama.
    Additionally, make a realtime call to the Polygon.io REST API to fetch a stock quote.

--- function-example/src/main/java/com/packt/spring/ai/examples/StockQuotesFunctionApplication.java
+++ function-example/src/main/java/com/packt/spring/ai/examples/StockQuotesFunctionApplication.java
@@ -15,35 +15,63 @@
  */
 package com.packt.spring.ai.examples;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.function.Function;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestClient;
+
+import lombok.Getter;
 
 /**
  * {@link SpringBootApplication} using Spring AI with Ollama (llama3.2 model) demonstrating Spring AI's Function Calling
  * to return a Stock Quote given a Stock Symbol (for examples: AAPL).
  *
  * @author John Blum
+ * @see java.util.function.Function
  * @see org.springframework.ai.chat.client.ChatClient
  * @see org.springframework.ai.chat.model.ChatModel
+ * @see org.springframework.ai.chat.prompt.ChatOptions
+ * @see org.springframework.ai.chat.prompt.Prompt
+ * @see org.springframework.ai.chat.prompt.PromptTemplate
  * @see org.springframework.boot.ApplicationRunner
  * @see org.springframework.boot.autoconfigure.SpringBootApplication
  * @see org.springframework.boot.builder.SpringApplicationBuilder
- * @since 0.1.0
+ * @see org.springframework.core.env.Environment
+ * @see org.springframework.web.client.RestClient
  */
 @SpringBootApplication
 @SuppressWarnings("unused")
 public class StockQuotesFunctionApplication {
 
 	private static final String EXIT = "exit";
+	private static final String STOCK_PRICE_FUNCTION_NAME = "StockPriceFunction";
 	private static final String USER_PROFILE = "user";
 
 	public static void main(String[] args) {
@@ -61,28 +89,79 @@ public class StockQuotesFunctionApplication {
 	}
 
 	@Bean
+	RestClient polygonClient(Environment environment) {
+
+		String baseUrl = environment.getRequiredProperty("examples.app.stock-quotes.polygon.api.base-url");
+
+		Consumer<HttpHeaders> defaultHttpHeaders = httpHeaders -> {
+			String polygonApiKey = environment.getRequiredProperty("examples.app.stock-quotes.polygon.api.key");
+			httpHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer %s".formatted(polygonApiKey));
+		};
+
+		return RestClient.builder()
+			.baseUrl(baseUrl)
+			.defaultHeaders(defaultHttpHeaders)
+			.build();
+	}
+
+	@Bean(STOCK_PRICE_FUNCTION_NAME)
+	@Description("Get the current price for a stock")
+	Function<StockQuote.Request, StockQuote.Response> stockQuoteFunction(Environment environment,
+			RestClient polygonClient) {
+
+		return request -> {
+
+			String exchangeSymbol = request.exchangeSymbol();
+
+			System.out.printf("Fetching quote for stock [%s]...%n", exchangeSymbol);
+
+			Map<String, ?> uriVariables = Map.of("exchangeSymbol", exchangeSymbol);
+
+			String uri = environment
+				.getRequiredProperty("examples.app.stock-quotes.polygon.api.market-data.previous-close.uri");
+
+			StockQuote stockQuote = polygonClient.get()
+				.uri(uri, uriVariables)
+				.accept(MediaType.APPLICATION_JSON)
+				.retrieve()
+				.body(StockQuote.class);
+
+			Assert.state(stockQuote != null,
+				() -> "Failed to retrieve quote for stock [%s]".formatted(request));
+
+			return new StockQuote.Response(stockQuote.getFirstResult().getClose());
+		};
+	}
+
+	@Bean
 	ApplicationRunner programRunner(ChatClient chatClient) {
 
 		return args -> {
 
 			Scanner scanner = new Scanner(System.in);
-			String input;
+			String stockSymbol;
 
 			System.out.print("Enter Stock Symbol: ");
 
-			while (isNotExit(input = scanner.nextLine())) {
+			while (isNotExit(stockSymbol = scanner.nextLine())) {
+				if (StringUtils.hasText(stockSymbol)) {
 
-				String stockSymbol = input;
+					ChatOptions ollamaChatOptions = OllamaOptions.builder()
+						.withFunction(STOCK_PRICE_FUNCTION_NAME)
+						.build();
 
-				Consumer<ChatClient.PromptUserSpec> promptUserSpecConsumer = promptUserSpec ->
-					promptUserSpec.text("What is the current stock price for {stock}?").param("stock", stockSymbol);
+					Map<String, Object> promptArguments = Map.of("stock", stockSymbol);
 
-				String stockPrice = chatClient.prompt()
-					.user(promptUserSpecConsumer)
-					.call()
-					.content();
+					String template = "What is the current price for {stock}?";
 
-				System.out.printf("[%s]: %s%n%n", stockSymbol, stockPrice);
+					Prompt prompt = new PromptTemplate(template)
+						.create(promptArguments, ollamaChatOptions);
+
+					String stockPrice = chatClient.prompt(prompt).call().content();
+
+					System.out.printf("AI> %s%n%n", stockPrice);
+				}
+
 				System.out.print("Enter Stock Symbol: ");
 			}
 		};
@@ -90,5 +169,80 @@ public class StockQuotesFunctionApplication {
 
 	private boolean isNotExit(String input) {
 		return StringUtils.hasText(input) && !EXIT.equalsIgnoreCase(input);
+	}
+
+	@Getter
+	static class StockQuote {
+
+		private Boolean adjusted;
+
+		private Integer count;
+		private Integer queryCount;
+		private Integer resultsCount;
+
+		@JsonProperty("request_id")
+		private String requestId;
+
+		private String status;
+		private String ticker;
+
+		@SuppressWarnings("all")
+		private List<Result> results = Collections.emptyList();
+
+		Result getFirstResult() {
+
+			boolean hasResults = resultsCount != null
+				&& resultsCount != 0
+				&& !CollectionUtils.isEmpty(results);
+
+			Assert.state(hasResults, () -> "Expected results in StockQuote [%s]".formatted(this));
+
+			return getResults().get(0);
+		}
+
+		@Override
+		public String toString() {
+			return getTicker();
+		}
+
+		record Request(String exchangeSymbol) {
+
+		}
+
+		record Response(BigDecimal price) {
+
+		}
+
+		@Getter
+		static class Result {
+
+			@JsonProperty("o")
+			private BigDecimal open;
+
+			@JsonProperty("c")
+			private BigDecimal close;
+
+			@JsonProperty("h")
+			private BigDecimal high;
+
+			@JsonProperty("l")
+			private BigDecimal low;
+
+			@JsonProperty("vw")
+			private BigDecimal volumeWeightAveragePrice;
+
+			@JsonProperty("n")
+			private BigInteger numberOfTransactions;
+
+			@JsonProperty("v")
+			private BigInteger volume;
+
+			@JsonProperty("t")
+			private Instant timestamp;
+
+			@JsonProperty("T")
+			private String ticker;
+
+		}
 	}
 }
