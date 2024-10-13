9e32fcec33998411aec609b5d32d8293ce5586b8
John Blum <jblum0@gmail.com>
2024-10-13 16:47:53 -0700

    Create AbstractBaseTextSplitter class supporting implementations of Spring AI's TextSplitter.
    The AbstractBaseTextSplitter encapsulates logic and functionality for pre-processing text and splitting text.

---
+++ similarity-search-example/src/main/java/com/packt/spring/ai/examples/similaritysearch/support/AbstractBaseTextSplitter.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.packt.spring.ai.examples.similaritysearch.support;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.util.StringUtils;
+
+/**
+ * Abstract base class supporting implementations of Spring AI's {@link TextSplitter}.
+ *
+ * @author John Blum
+ * @see org.springframework.ai.transformer.splitter.TextSplitter
+ * @since 0.1.0
+ */
+public abstract class AbstractBaseTextSplitter extends TextSplitter {
+
+	protected static final String EMPTY_STRING = "";
+	protected static final String SINGLE_SPACE = " ";
+	protected static final String WHITESPACE_REGEX = "\\s+";
+
+	protected String preProcess(String text) {
+		return text.trim();
+	}
+
+	protected abstract String regex();
+
+	protected String resolveRegex(String text) {
+		String regex = regex();
+		return StringUtils.hasText(regex) ? regex : text;
+	}
+
+	@Override
+	protected List<String> splitText(String text) {
+
+		return StringUtils.hasText(text)
+			? Arrays.asList(preProcess(text).split(resolveRegex(text)))
+			: Collections.emptyList();
+	}
+}
