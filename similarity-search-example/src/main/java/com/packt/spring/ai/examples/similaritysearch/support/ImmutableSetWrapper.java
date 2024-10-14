e91f69d67eafd39029a220ba50dffa0df692bd98
John Blum <jblum0@gmail.com>
2024-10-13 17:00:39 -0700

    Create ImmutableSetWrapper class wrapping an existing Set, making the wrapped Set immutable and providing the ifNonEmpty(:Consumer<Set<E>>) and isNotEmpty() operations.

---
+++ similarity-search-example/src/main/java/com/packt/spring/ai/examples/similaritysearch/support/ImmutableSetWrapper.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.packt.spring.ai.examples.similaritysearch.support;
+
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+/**
+ * Immutable {@link Set} implementation wrapping and decorating an existing {@link Set}.
+ *
+ * @author John Blum
+ * @see java.util.AbstractSet
+ * @see java.util.Set
+ * @since 0.1.0
+ */
+@Getter(AccessLevel.PROTECTED)
+public class ImmutableSetWrapper<E> extends AbstractSet<E> {
+
+	static <E> ImmutableSetWrapper<E> from(Set<E> set) {
+		return new ImmutableSetWrapper<>(set);
+	}
+
+	private final Set<E> set;
+
+	ImmutableSetWrapper(Set<E> set) {
+		Assert.notNull(set, "Set to wrap is required");
+		this.set = set;
+	}
+
+	public void ifNotEmpty(Consumer<Set<E>> consumer) {
+		if (isNotEmpty()) {
+			consumer.accept(this);
+		}
+	}
+
+	public boolean isNotEmpty() {
+		return !isEmpty();
+	}
+
+	@Override
+	public @NonNull Iterator<E> iterator() {
+		return getSet().iterator();
+	}
+
+	@Override
+	public int size() {
+		return getSet().size();
+	}
+}
