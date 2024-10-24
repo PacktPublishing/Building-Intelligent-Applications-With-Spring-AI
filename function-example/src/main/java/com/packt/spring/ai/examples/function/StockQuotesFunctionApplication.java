81eda459d8c7f48fa86a421fc9c095bd073c331f
John Blum <jblum2@apple.com>
2024-10-24 14:55:31 -0700

    Move StockQuotesFunctionApplication to com.packt.spring.ai.examples.function.

--- function-example/src/main/java/com/packt/spring/ai/examples/function/StockQuotesFunctionApplication.java
+++ function-example/src/main/java/com/packt/spring/ai/examples/function/StockQuotesFunctionApplication.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.packt.spring.ai.examples;
+package com.packt.spring.ai.examples.function;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
