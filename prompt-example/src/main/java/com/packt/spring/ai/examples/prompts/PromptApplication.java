8ee10fb7544a1009560d97d255e4bf7eb4eb48da
John Blum <jblum2@apple.com>
2024-10-24 14:57:56 -0700

    Move PromptApplication to com.packt.spring.ai.examples.prompts.PromptApplication.

--- prompt-example/src/main/java/com/packt/spring/ai/examples/prompts/PromptApplication.java
+++ prompt-example/src/main/java/com/packt/spring/ai/examples/prompts/PromptApplication.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.packt.spring.ai.exmaples.prompts;
+package com.packt.spring.ai.examples.prompts;
 
 import java.util.List;
 
