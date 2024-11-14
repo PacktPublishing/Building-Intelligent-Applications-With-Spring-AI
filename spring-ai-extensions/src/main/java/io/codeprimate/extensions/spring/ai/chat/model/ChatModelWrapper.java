5546ac0ce7c567c9f3a87d6182bdcc0b5a10cafe
John Blum <jblum0@gmail.com>
2024-11-13 22:49:43 -0800

    Create ChatModelWrapper class to wrap an existing ChatModel.

---
+++ spring-ai-extensions/src/main/java/io/codeprimate/extensions/spring/ai/chat/model/ChatModelWrapper.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright 2024 Author or Authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.codeprimate.extensions.spring.ai.chat.model;
+
+import org.cp.elements.lang.Assert;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+/**
+ * Spring AI {@link ChatModel} implementation and wrapper for an existing {@link ChatModel}.
+ *
+ * @author John Blum
+ * @see org.springframework.ai.chat.model.ChatModel
+ * @since 0.1.0
+ */
+@Getter(AccessLevel.PROTECTED)
+public class ChatModelWrapper implements ChatModel {
+
+	public static ChatModelWrapper from(ChatModel chatModel) {
+		return new ChatModelWrapper(chatModel);
+	}
+
+	private final ChatModel chatModel;
+
+	protected ChatModelWrapper(ChatModel chatModel) {
+		Assert.notNull(chatModel, "ChatModel is required");
+		this.chatModel = chatModel;
+	}
+
+	@Override
+	public ChatResponse call(Prompt prompt) {
+		return doAfter(getChatModel().call(doBefore(prompt)));
+	}
+
+	protected Prompt doBefore(Prompt prompt) {
+		return prompt;
+	}
+
+	protected ChatResponse doAfter(ChatResponse chatResponse) {
+		return chatResponse;
+	}
+
+	@Override
+	public ChatOptions getDefaultOptions() {
+		return getChatModel().getDefaultOptions();
+	}
+}
