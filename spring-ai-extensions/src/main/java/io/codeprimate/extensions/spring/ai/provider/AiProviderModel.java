94e17b8d1db8f4ac0c579d0f7aa55f7e82c626bf
John Blum <jblum0@gmail.com>
2024-12-09 21:34:20 -0800

    Create AiProviderModel record to associate an AiProvider with a Spring AI Model.

---
+++ spring-ai-extensions/src/main/java/io/codeprimate/extensions/spring/ai/provider/AiProviderModel.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.codeprimate.extensions.spring.ai.provider;
+
+import io.codeprimate.extensions.spring.ai.provider.model.ModelNameResolver;
+import io.codeprimate.extensions.spring.ai.provider.support.SpringAiProvider;
+
+import org.cp.elements.lang.Assert;
+import org.springframework.ai.model.Model;
+
+/**
+ * Java Record used to model a {@link AiProvider} and the provider (Spring AI-based) {@link Model}.
+ *
+ * @author John Blum
+ * @param aiProvider {@link AiProvider} to associate with its {@link Model}.
+ * @param model {@link Model} provided by the given {@link AiProvider}.
+ * @see io.codeprimate.extensions.spring.ai.provider.AiProvider
+ * @see org.springframework.ai.model.Model
+ */
+@SuppressWarnings("unused")
+public record AiProviderModel(AiProvider aiProvider, Model<?, ?> model) implements AiProvider {
+
+	public static AiProviderModel from(Model<?, ?> model) {
+
+		AiProvider aiProvider = SpringAiProvider.findByModel(model)
+			.orElseThrow(() -> AiProviderNotFoundException.from(model));
+
+		return new AiProviderModel(aiProvider, model);
+	}
+
+	public AiProviderModel {
+		Assert.notNull(aiProvider, "AiProvider is required");
+		Assert.notNull(model, "AiProvider [%s] Model is required", aiProvider.getName());
+	}
+
+	private String getModelName(Model<?, ?> model) {
+		return ModelNameResolver.defaultModelNameResolver().resolveName(model);
+	}
+
+	@Override
+	public String getName() {
+		return aiProvider().getName();
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends Model<?, ?>> T getTypedModel() {
+		return (T) model();
+	}
+}
