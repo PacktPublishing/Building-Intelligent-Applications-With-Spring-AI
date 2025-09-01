ed181f819760d491575c641fe521a5eebdd50f2b
John Blum <jblum0@gmail.com>
2025-09-01 02:21:50 -0700

    Define ADT for a chat session

---
+++ realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/model/ChatSession.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.chat.model;
+
+import java.util.Iterator;
+import java.util.UUID;
+
+import io.packt.spring.ai.examples.app.chat.util.ChatUserNotFoundException;
+
+import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * Abstract Data Type (ADT) modeling a chat session with users communicating in chat.
+ *
+ * @author John Blum
+ * @see java.lang.Iterable
+ * @see ChatUser
+ * @since 0.1.0
+ */
+@ToString(of = "id")
+@Getter(AccessLevel.PUBLIC)
+@SuppressWarnings("unused")
+public class ChatSession implements Iterable<ChatUser> {
+
+	public static ChatSession withUser(ChatUser user) {
+		ChatSession session = new ChatSession(UUID.randomUUID());
+		session.add(user);
+		return session;
+	}
+
+	private final UUID id;
+
+	private final ChatMessages chatMessages;
+
+	private final ChatUsers chatUsers;
+
+	public ChatSession(UUID id) {
+
+		Assert.notNull(id, "Chat Session ID is required");
+
+		this.id = id;
+		this.chatMessages = ChatMessages.empty().mutable();
+		this.chatUsers = ChatUsers.empty().mutable();
+	}
+
+	public boolean isActive() {
+		return getChatUsers().isNotEmpty();
+	}
+
+	public boolean add(ChatUser user) {
+		return getChatUsers().add(user);
+	}
+
+	public boolean post(ChatMessage chatMessage) {
+		assertChatMessage(chatMessage);
+		return getChatMessages().add(chatMessage);
+	}
+
+	private void assertChatMessage(ChatMessage chatMessage) {
+
+		Assert.notNull(chatMessage, "ChatMessage is required");
+
+		ChatUser user = chatMessage.user();
+
+		try {
+			getChatUsers().findBy(user.id());
+		}
+		catch (ChatUserNotFoundException cause) {
+
+		}
+
+	}
+
+	public boolean remove(ChatUser user) {
+		return getChatUsers().remove(user);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof ChatSession that)) {
+			return false;
+		}
+
+		return this.getId().equals(that.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return System.identityHashCode(getId());
+	}
+
+	@Override
+	public @NonNull Iterator<ChatUser> iterator() {
+		return ChatUsers.of(this.chatUsers).iterator();
+	}
+}
