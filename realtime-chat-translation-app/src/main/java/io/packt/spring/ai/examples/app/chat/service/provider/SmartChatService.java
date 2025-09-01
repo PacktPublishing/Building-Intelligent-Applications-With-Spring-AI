01c8d5b60ddd2fae5d2a42c2425173ce95e8d300
John Blum <jblum0@gmail.com>
2025-09-01 02:30:48 -0700

    Define SmartChatService provider implementation using AI to enhance all chat application operations

---
+++ realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/service/provider/SmartChatService.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.chat.service.provider;
+
+import java.util.UUID;
+
+import io.packt.spring.ai.examples.app.chat.model.AudioMessage;
+import io.packt.spring.ai.examples.app.chat.model.ChatMessage;
+import io.packt.spring.ai.examples.app.chat.model.ChatSession;
+import io.packt.spring.ai.examples.app.chat.model.ChatSessions;
+import io.packt.spring.ai.examples.app.chat.model.ChatUser;
+import io.packt.spring.ai.examples.app.chat.model.ChatUsers;
+import io.packt.spring.ai.examples.app.chat.model.IsoLanguage;
+import io.packt.spring.ai.examples.app.chat.service.AbstractChatService;
+import io.packt.spring.ai.examples.app.chat.service.ChatService;
+import io.packt.spring.ai.examples.app.chat.service.LanguageTranslator;
+import io.packt.spring.ai.examples.app.chat.service.TextToSpeechSynthesizer;
+import io.packt.spring.ai.examples.app.chat.util.InvalidChatSessionException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Spring {@link Service} implementing {@link ChatService} managing chat sessions in-memory
+ * and using AI for real-time chat translation.
+ *
+ * @author John Blum
+ * @see org.springframework.ai.chat.client.ChatClient
+ * @see AbstractChatService
+ * @see ChatUsers
+ * @see Service
+ * @since 0.1.0
+ */
+@Service
+@RequiredArgsConstructor
+@Getter(AccessLevel.PROTECTED)
+public class SmartChatService extends AbstractChatService {
+
+	private final ChatSessions chatSessions = ChatSessions.empty().mutable();
+
+	private final LanguageTranslator languageTranslator;
+
+	private final TextToSpeechSynthesizer speechSynthesizer;
+
+	@Override
+	public ChatSession findBy(UUID chatSessionId) {
+		return getChatSessions().findBy(chatSessionId);
+	}
+
+	@Override
+	public ChatSession joinChatSession(UUID chatSessionId, ChatUser user) {
+
+		ChatSession chatSession = findBy(chatSessionId);
+
+		if (!chatSession.add(user)) {
+			throw InvalidChatSessionException.from(chatSession, user);
+		}
+
+		return chatSession;
+	}
+
+	@Override
+	public ChatSession newChatSession(ChatUser user) {
+		ChatSession session = ChatSession.withUser(user);
+		getChatSessions().add(session);
+		return session;
+	}
+
+	@Override
+	public AudioMessage textToSpeech(ChatMessage chatMessage) {
+
+		Assert.notNull(chatMessage, "ChatMessage is required");
+
+		String text = chatMessage.message();
+		byte[] audioData = getSpeechSynthesizer().speak(text);
+
+		return AudioMessage.from(audioData);
+	}
+
+	@Override
+	public ChatMessage translateChatMessage(ChatMessage message, IsoLanguage language) {
+
+		Assert.notNull(message, "ChatMessage is required");
+		Assert.notNull(language, "Language is required");
+
+		if (message.language().isNotEqualTo(language)) {
+			return message.findBy(language).orElseGet(() -> {
+				String originalMessage = message.message();
+				String translatedMessage = getLanguageTranslator().translate(originalMessage, language);
+				ChatMessage translatedChatMessage = ChatMessage.from(message.user(), language, translatedMessage);
+				message.add(translatedChatMessage);
+				return translatedChatMessage;
+			});
+		}
+
+		return message;
+	}
+}
