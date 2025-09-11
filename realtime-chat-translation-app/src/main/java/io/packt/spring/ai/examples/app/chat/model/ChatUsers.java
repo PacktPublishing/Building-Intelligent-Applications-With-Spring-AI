57dcc7a1f8af8fb675c429073ad8a3328bae1d0c
John Blum <jblum0@gmail.com>
2025-09-11 16:39:38 -0700

    Add toSet() method to ChatUsers

--- realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/model/ChatUsers.java
+++ realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/model/ChatUsers.java
@@ -20,9 +20,11 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -129,5 +131,9 @@ public interface ChatUsers extends Iterable<ChatUser> {
 
 	default List<ChatUser> toList() {
 		return stream().toList();
+	}
+
+	default Set<ChatUser> toSet() {
+		return stream().collect(Collectors.toSet());
 	}
 }
