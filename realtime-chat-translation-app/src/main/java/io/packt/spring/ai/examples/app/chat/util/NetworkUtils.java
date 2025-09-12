025b04fed2d3e239d712f2223b78fec05636676b
John Blum <jblum0@gmail.com>
2025-09-12 14:06:47 -0700

    Refactor resolveLocalhostIpAddress(..) method to take Spring Environment in NetworkUtils

--- realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/util/NetworkUtils.java
+++ realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/util/NetworkUtils.java
@@ -15,10 +15,20 @@
  */
 package io.packt.spring.ai.examples.app.chat.util;
 
-import java.net.InetSocketAddress;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.URI;
 import java.net.URL;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.cp.elements.function.ThrowableSupplier;
+import org.springframework.core.env.Environment;
 
 /**
  * Abstract utility class for processing {@link URL} objects.
@@ -30,23 +40,71 @@ public abstract class NetworkUtils {
 
 	public static final int DEFAULT_SERVER_PORT = 8080;
 
-	public static final String LOCALHOST = "localhost";
+	public static final String NETWORK_IP_ADDRESS_PREFIX_PROPERTY = "app.network.host.ip-address-prefix";
 	public static final String WEB_PATH_SEPARATOR = "/";
 
-	public static String resolveIpAddress(String hostname, int port) {
-		return InetSocketAddress.createUnresolved(hostname, port).getHostName();
+	@SuppressWarnings("unused")
+	public static String resolveHostIpAddress(String hostname) {
+		return getSafely(() -> InetAddress.getByName(hostname).getHostAddress(), cause -> {
+			String message = "Failed to resolve host's [%s] IP address".formatted(hostname);
+			throw new IllegalStateException(message, cause);
+		});
 	}
 
-	public static String resolveIpAddress(String hostname) {
-		return resolveIpAddress(hostname, DEFAULT_SERVER_PORT);
+	public static String resolveLocalhostIpAddress(Environment environment) {
+
+		String networkIpAddressPrefix = environment.getProperty(NETWORK_IP_ADDRESS_PREFIX_PROPERTY);
+
+		List<NetworkInterface> networkInterfaces = streamNetworkInterfaces()
+			.filter(networkInterfacePredicate())
+			.toList();
+
+		for (NetworkInterface networkInterface : networkInterfaces) {
+			Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+			while (addresses.hasMoreElements()) {
+				InetAddress address = addresses.nextElement();
+				String hostAddress = nullSafeHostAddress(address);
+				if (hostAddress.startsWith(networkIpAddressPrefix)) {
+					return hostAddress;
+				}
+			}
+		}
+
+		throw new IllegalStateException("Failed to resolve localhost IP address");
 	}
 
-	public static String resolveLocalhostIpAddress() {
-		return resolveIpAddress(LOCALHOST);
+	private static Predicate<NetworkInterface> networkInterfacePredicate() {
+
+		return networkInterface -> {
+			try {
+				return networkInterface.isUp()
+					&& isNot(networkInterface.isLoopback())
+					&& isNot(networkInterface.isVirtual());
+			}
+			catch (SocketException e) {
+				return false;
+			}
+		};
 	}
 
-	public static URI resolveUri(String baseUrl, Object... elements) {
-		return URI.create(baseUrl.formatted(elements));
+	private static String nullSafeHostAddress(InetAddress address) {
+		return String.valueOf(address.getHostAddress());
+	}
+
+	private static Stream<NetworkInterface> streamNetworkInterfaces() {
+		return getSafely(NetworkInterface::networkInterfaces, cause -> {
+			throw new IllegalStateException("Failed to resolve NetworkInterfaces", cause);
+		});
+	}
+
+	public static String resolveLocalhostName() {
+		return getSafely(() -> InetAddress.getLocalHost().getHostName(), cause -> {
+			throw new IllegalStateException("Failed to resolve localhost name", cause);
+		});
+	}
+
+	public static URI resolveUri(String baseUrl, Object... pathElements) {
+		return URI.create(baseUrl.formatted(pathElements));
 	}
 
 	public static URL toUrl(URI uri) {
@@ -57,6 +115,19 @@ public abstract class NetworkUtils {
 		catch (MalformedURLException cause) {
 			String message = "Failed to construct URL from URI [%s]".formatted(uri);
 			throw new IllegalArgumentException(message, cause);
+		}
+	}
+
+	private static boolean isNot(boolean condition) {
+		return !condition;
+	}
+
+	private static <T> T getSafely(ThrowableSupplier<T> supplier, Function<Exception, T> exceptionHandler) {
+		try {
+			return supplier.get();
+		}
+		catch (Exception cause) {
+			return exceptionHandler.apply(cause);
 		}
 	}
 }
