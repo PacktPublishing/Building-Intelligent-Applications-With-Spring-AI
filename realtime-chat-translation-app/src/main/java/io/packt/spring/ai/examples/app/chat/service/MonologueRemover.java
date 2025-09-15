ed5afeec24648565b289d1c292ac612a3eb65b47
John Blum <jblum0@gmail.com>
2025-09-14 21:50:07 -0700

    Create MonologueRemove functional interface defining a contract to remove monologugin from text messages

---
+++ realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/service/MonologueRemover.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.chat.service;
+
+import io.packt.spring.ai.examples.app.chat.model.TextMessage;
+
+/**
+ * {@link FunctionalInterface} used to remove monologue from a {@link TextMessage}.
+ *
+ * @author John Blum
+ * @see TextMessage
+ * @since 0.1.0
+ */
+@FunctionalInterface
+public interface MonologueRemover {
+
+	TextMessage removeMonologue(TextMessage message);
+
+}
