89b57fb756cd8029bca2b1cdedcdef06a830f266
John Blum <jblum0@gmail.com>
2025-09-20 17:05:10 -0700

    Add translate(:ChatMessage, :IsoLanguage) and translate(:TextMessage, :IsoLanguage) methods to LanguageTranslator

--- realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/service/LanguageTranslator.java
+++ realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/service/LanguageTranslator.java
@@ -15,7 +15,9 @@
  */
 package io.packt.spring.ai.examples.app.chat.service;
 
+import io.packt.spring.ai.examples.app.chat.model.ChatMessage;
 import io.packt.spring.ai.examples.app.chat.model.IsoLanguage;
+import io.packt.spring.ai.examples.app.chat.model.TextMessage;
 
 /**
  * {@link FunctionalInterface} defining a contract to translate a {@link String message}
@@ -31,6 +33,21 @@ public interface LanguageTranslator {
 
 	default String translate(String text, IsoLanguage language) {
 		return translate(text, IsoLanguage.fromDefault(), language);
+	}
+
+	default ChatMessage translate(ChatMessage message, IsoLanguage language) {
+
+		String translatedText = translate(message.message(), message.language(), language);
+
+		return ChatMessage.from(message)
+			.with(translatedText)
+			.in(language)
+			.build();
+	}
+
+	default TextMessage translate(TextMessage message, IsoLanguage language) {
+		String translatedText = translate(message.getText(), message.getLanguage(), language);
+		return TextMessage.from(translatedText, language);
 	}
 
 	String translate(String text, IsoLanguage inLanguage, IsoLanguage toLanguage);
--- realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/service/provider/SmartChatService.java
+++ realtime-chat-translation-app/src/main/java/io/packt/spring/ai/examples/app/chat/service/provider/SmartChatService.java
@@ -109,16 +109,11 @@ public class SmartChatService extends AbstractChatService {
 		Assert.notNull(message, "ChatMessage is required");
 		Assert.notNull(language, "Language is required");
 
-		IsoLanguage messageLanguage = message.language();
-
-		if (messageLanguage.isNotEqualTo(language)) {
+		if (message.isNotInLanguage(language)) {
 			return message.findBy(language).orElseGet(() -> {
-				String originalMessage = message.message();
-				String translatedMessage = getLanguageTranslator().translate(originalMessage, messageLanguage, language);
-				ChatMessage translatedChatMessage =
-					ChatMessage.from(message).with(translatedMessage).in(language).build();
-				message.add(translatedChatMessage);
-				return translatedChatMessage;
+				ChatMessage translatedMessage = getLanguageTranslator().translate(message, language);
+				message.add(translatedMessage);
+				return translatedMessage;
 			});
 		}
 
