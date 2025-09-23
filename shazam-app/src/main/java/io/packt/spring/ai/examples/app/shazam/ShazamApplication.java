897f9507ac1dd286f003f215e86aefed6aaa351e
John Blum <jblum0@gmail.com>
2025-09-22 21:39:02 -0700

    Add base implementation and setup for the Shazam Spring Boot application

---
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/ShazamApplication.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.shazam;
+
+import io.codeprimate.extensions.spring.boot.AbstractDesktopSpringBootApplication;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+/**
+ * {@link SpringBootApplication} implementing the {@literal Shazam} app called {@literal What's My Jam}.
+ *
+ * @author John Blum
+ * @see SpringBootApplication
+ * @see AbstractDesktopSpringBootApplication
+ * @since 0.1.0
+ */
+@SpringBootApplication
+@SuppressWarnings("unused")
+public class ShazamApplication extends AbstractDesktopSpringBootApplication {
+
+	public static void main(String[] args) {
+		runSpringServletWebApplication(ShazamApplication.class, asStringArray(), args);
+	}
+
+	@Bean
+	ApplicationRunner programRunner(Environment environment) {
+		return applicationArguments -> getLogger().info("GOTO URL [{}]", getWebApplicationUrl(environment));
+	}
+}
