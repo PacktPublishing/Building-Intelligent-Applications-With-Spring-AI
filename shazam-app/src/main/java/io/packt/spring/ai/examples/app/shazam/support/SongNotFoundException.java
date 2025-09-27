e1f55bfbbc7982c50c39f6d66b3bc24532107564
John Blum <jblum0@gmail.com>
2025-09-26 23:10:06 -0700

    Create SongNotFoundException (RuntimeException) which is thrown when a song is not found during search

---
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/support/SongNotFoundException.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.shazam.support;
+
+import io.packt.spring.ai.examples.app.shazam.model.Song;
+
+/**
+ * Java {@link RuntimeException} thrown when a {@link Song} could not be found during a search.
+ *
+ * @author John Blum
+ * @see RuntimeException
+ * @since 0.1.0
+ */
+@SuppressWarnings("unused")
+public class SongNotFoundException extends RuntimeException {
+
+	public SongNotFoundException() {
+
+	}
+
+	public SongNotFoundException(String message) {
+		super(message);
+	}
+
+	public SongNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+	public SongNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
