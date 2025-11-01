9b2acd056c6f13eb734d963a0ba9423cbda050c5
John Blum <jblum0@gmail.com>
2025-11-01 16:23:00 -0700

    Refactor SmartMusicService
    * Declare @PostConstruct init() method to log the configured VectorStore implementation
    * Configure similarity search threshold and topK in search(:Audio) method when building SearchRequest
    * Resolve the Song Identifiers (IDs) from the matching (song) documents in search(:Audio) method
    * Associate Song Identifier (ID) with Documents returned by audio split before vectorization in store(:Song) method

--- shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/service/provider/SmartMusicService.java
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/service/provider/SmartMusicService.java
@@ -15,8 +15,13 @@
  */
 package io.packt.spring.ai.examples.app.shazam.service.provider;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+import io.packt.spring.ai.examples.app.shazam.config.SongSearchProperties;
 import io.packt.spring.ai.examples.app.shazam.model.Audio;
 import io.packt.spring.ai.examples.app.shazam.model.Song;
 import io.packt.spring.ai.examples.app.shazam.service.AudioSplitter;
@@ -24,6 +29,7 @@ import io.packt.spring.ai.examples.app.shazam.service.MusicService;
 import io.packt.spring.ai.examples.app.shazam.support.SongNotFoundException;
 import io.packt.spring.ai.examples.app.shazam.support.UuidIdGenerator;
 
+import org.slf4j.Logger;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -31,9 +37,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * {@link MusicService} implementation using AI.
@@ -44,14 +52,29 @@ import lombok.RequiredArgsConstructor;
  * @see org.springframework.stereotype.Service
  * @since 0.1.0
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Getter(AccessLevel.PROTECTED)
+@SuppressWarnings("unused")
 public class SmartMusicService implements MusicService {
+
+	private static final String SONG_ID_KEY = "songId";
 
 	private final AudioSplitter audioSplitter;
 
+	private final SongSearchProperties songSearchProperties;
+
 	private final VectorStore vectorStore;
+
+	protected Logger getLogger() {
+		return log;
+	}
+
+	@PostConstruct
+	public void afterInit() {
+		getLogger().info("Using VectorStore [{}]", getVectorStore().getClass().getName());
+	}
 
 	@Override
 	public Song search(Audio audio) {
@@ -60,8 +83,11 @@ public class SmartMusicService implements MusicService {
 
 		SearchRequest searchRequest = buildSearchRequest(audio);
 		List<Document> songMatches = getVectorStore().similaritySearch(searchRequest);
+		Set<UUID> songIds = resolveSongIdentifiers(songMatches);
 
-		// TODO: Lookup Song in database based on matching Documents
+		if (!songIds.isEmpty()) {
+			// TODO: Lookup Song in database based on matching Documents
+		}
 
 		throw new SongNotFoundException("No song was found with the given audio");
 	}
@@ -69,8 +95,29 @@ public class SmartMusicService implements MusicService {
 	private SearchRequest buildSearchRequest(Audio audio) {
 
 		return SearchRequest.builder()
+			.similarityThreshold(getSongSearchProperties().resolveSimilarityThreshold())
+			.topK(getSongSearchProperties().resolveTopK())
 			.query(audio.encode())
 			.build();
+	}
+
+	private UUID assertSongIdentifier(Object songId) {
+		Assert.isInstanceOf(UUID.class, songId, () -> "Expected Song ID [%s] to be a UUID".formatted(songId));
+		return (UUID) songId;
+	}
+
+	private <T> List<T> nullSafeList(List<T> list) {
+		return list != null ? list : Collections.emptyList();
+	}
+
+	private UUID resolveSongIdentifier(Document document) {
+		return assertSongIdentifier(document.getMetadata().get(SONG_ID_KEY));
+	}
+
+	private Set<UUID> resolveSongIdentifiers(List<Document> songMatches) {
+		return nullSafeList(songMatches).stream()
+			.map(this::resolveSongIdentifier)
+			.collect(Collectors.toSet());
 	}
 
 	@Override
@@ -79,9 +126,20 @@ public class SmartMusicService implements MusicService {
 		Assert.notNull(song, "Song is required");
 
 		List<Document> documents = getAudioSplitter().split(song);
-		List<Document> identifiedDocuments = documents.stream().map(this::identify).toList();
+
+		List<Document> identifiedDocuments = documents.stream()
+			.map(document -> associateSong(document, song))
+			.map(this::identify)
+			.toList();
 
 		getVectorStore().accept(identifiedDocuments);
+
+		// TODO: Store Song in database
+	}
+
+	private Document associateSong(Document document, Song song) {
+		document.getMetadata().put(SONG_ID_KEY, song.getId());
+		return document;
 	}
 
 	private boolean isIdentified(Document document) {
