d6602beae847cc7d51d93a3a040a7911dc2032d9
John Blum <jblum0@gmail.com>
2025-11-03 11:45:48 -0800

    Create NonUniqueSongException, Java RuntimeException thrown when search matches multiple songs

---
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/support/NonUniqueSongException.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.shazam.support;
+
+import java.util.Set;
+import java.util.UUID;
+
+import io.packt.spring.ai.examples.app.shazam.model.Song;
+
+/**
+ * Java {@link RuntimeException} thrown when more than 1 {@link Song} matches a search.
+ *
+ * @author John Blum
+ * @see RuntimeException
+ * @see Song
+ * @since 0.1.0
+ */
+@SuppressWarnings("unused")
+public class NonUniqueSongException extends RuntimeException {
+
+	public static NonUniqueSongException with(Set<UUID> songIdentifiers, Throwable cause) {
+		String message = "Audio matched multiple songs having identifiers %s".formatted(songIdentifiers);
+		return new NonUniqueSongException(message, cause);
+	}
+
+	public NonUniqueSongException() {
+
+	}
+
+	public NonUniqueSongException(String message) {
+		super(message);
+	}
+
+	public NonUniqueSongException(Throwable cause) {
+		super(cause);
+	}
+
+	public NonUniqueSongException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
