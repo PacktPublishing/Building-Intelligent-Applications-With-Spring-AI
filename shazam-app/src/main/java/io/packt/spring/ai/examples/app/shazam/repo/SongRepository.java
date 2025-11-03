eb7bff4457d1b3b24516e82e635f44beeb449885
John Blum <jblum0@gmail.com>
2025-11-03 12:15:19 -0800

    Create SongRepository interface, Data Access Object and Spring Data Repository backed by JPA to perform CRUD and simple query data access operations on Songs

---
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/repo/SongRepository.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.shazam.repo;
+
+import java.util.UUID;
+
+import io.packt.spring.ai.examples.app.shazam.model.Song;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Data Access Object (DAO) and Spring Data Repository used to perform basic CRUD (CREATE, READ, UPDATE & DELETE)
+ * along with simple query data access operations on {@link Song Songs}.
+ *
+ * @author John Blum
+ * @see CrudRepository
+ * @see Song
+ * @see UUID
+ * @since 0.1.0
+ */
+@SuppressWarnings("unused")
+public interface SongRepository extends CrudRepository<Song, UUID> {
+
+}
