98c7e9cb457b3abfaf171968ac8a986f4d30621d
John Blum <jblum0@gmail.com>
2025-11-30 18:04:36 -0800

    Declare TokenCountBasedBatchingStrategy Spring Bean in ShazamConfiguration

--- shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/config/ShazamConfiguration.java
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/config/ShazamConfiguration.java
@@ -15,10 +15,14 @@
  */
 package io.packt.spring.ai.examples.app.shazam.config;
 
+import com.knuddels.jtokkit.api.EncodingType;
+
 import io.codeprimate.extensions.spring.ai.config.EnableChatClient;
 import io.codeprimate.extensions.spring.boot.web.contoller.AdminController;
 import io.packt.spring.ai.examples.app.shazam.model.Song;
 
+import org.springframework.ai.embedding.BatchingStrategy;
+import org.springframework.ai.embedding.TokenCountBatchingStrategy;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -44,5 +48,10 @@ public class ShazamConfiguration {
 	@Bean
 	AdminController adminController() {
 		return new AdminController();
+	}
+
+	@Bean
+	BatchingStrategy tokenCountBasedBatchingStrategy() {
+		return new TokenCountBatchingStrategy(EncodingType.CL100K_BASE, 100_000, 0.1d);
 	}
 }
