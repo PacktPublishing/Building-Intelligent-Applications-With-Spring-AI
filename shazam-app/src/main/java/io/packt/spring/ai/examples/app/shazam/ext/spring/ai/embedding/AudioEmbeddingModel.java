7291c4af882c229e56cf707241bb2d8549c21d8d
John Blum <jblum0@gmail.com>
2025-12-18 10:32:52 -0800

    Create Spring AI EmbeddingModel implementation for Audio analysis and vectorization

---
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/ext/spring/ai/embedding/AudioEmbeddingModel.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.shazam.ext.spring.ai.embedding;
+
+import java.util.Base64;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+import io.packt.spring.ai.examples.app.shazam.ext.tarsos.dsp.AudioDispatcherBuilder;
+import io.packt.spring.ai.examples.app.shazam.model.Audio;
+
+import org.cp.elements.lang.Assert;
+import org.springframework.ai.chat.metadata.EmptyUsage;
+import org.springframework.ai.content.Media;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.Embedding;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.embedding.EmbeddingRequest;
+import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.ai.embedding.EmbeddingResponseMetadata;
+import org.springframework.lang.NonNull;
+import org.springframework.util.MimeTypeUtils;
+
+import be.tarsos.dsp.AudioDispatcher;
+
+/**
+ * {@link EmbeddingModel} implementation based on the {@literal TarsosDSP} library.
+ *
+ * @author John Blum
+ * @see Audio
+ * @see AudioDispatcher
+ * @see Document
+ * @see Embedding
+ * @see EmbeddingModel
+ * @see Media
+ * @since 0.1.0
+ */
+@SuppressWarnings("unused")
+public class AudioEmbeddingModel implements EmbeddingModel {
+
+	public static final int DEFAULT_VECTOR_DIMENSIONS = 53;
+	private static final int DEFAULT_INDEX = 0;
+
+	private static final String EMPTY_STRING = "";
+	private static final String EMBEDDING_MODEL = "mfcc";
+
+	@Override
+	public @NonNull EmbeddingResponse call(@NonNull EmbeddingRequest request) {
+		Document document = toDocument(request);
+		float[] embedding = embed(document);
+		return toEmbeddingResponse(embedding);
+	}
+
+	@Override
+	public int dimensions() {
+		return DEFAULT_VECTOR_DIMENSIONS;
+	}
+
+	@Override
+	public @NonNull float[] embed(@NonNull Document document) {
+
+		AtomicReference<float[]> mfcc = new AtomicReference<>();
+
+		Audio audio = toAudio(document);
+
+		AudioDispatcher audioDispatcher = AudioDispatcherBuilder.from(audio)
+			.withNumberOfCoefficients(dimensions())
+			.registerMFCC(mfcc::set)
+			.build();
+
+		audioDispatcher.run();
+
+		return mfcc.get();
+	}
+
+	private Audio toAudio(Document document) {
+
+		Assert.notNull(document, "Document is required");
+
+		Media media = document.getMedia();
+		byte[] data = media.getDataAsByteArray();
+
+		return Audio.from(data);
+	}
+
+	private Document toDocument(EmbeddingRequest request) {
+
+		List<String> instructions = request.getInstructions();
+		String text = String.join(EMPTY_STRING, instructions);
+		byte[] data = Base64.getDecoder().decode(text);
+
+		return Document.builder()
+			.media(toMedia(data))
+			.build();
+	}
+
+	private Embedding toEmbedding(float[] vector) {
+		return toEmbedding(vector, DEFAULT_INDEX);
+	}
+
+	private Embedding toEmbedding(float[] vector, int index) {
+		return new Embedding(vector, index);
+	}
+
+	private EmbeddingResponse toEmbeddingResponse(float[] vector) {
+		Embedding embedding = toEmbedding(vector);
+		List<Embedding> embeddings = List.of(embedding);
+		return new EmbeddingResponse(embeddings, withEmbeddingResponseMetadata());
+	}
+
+	private EmbeddingResponseMetadata withEmbeddingResponseMetadata() {
+		return new EmbeddingResponseMetadata(EMBEDDING_MODEL, new EmptyUsage());
+	}
+
+	private Media toMedia(byte[] data) {
+		return Media.builder()
+			.mimeType(MimeTypeUtils.APPLICATION_OCTET_STREAM)
+			.data(data)
+			.build();
+	}
+}
