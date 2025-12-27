249cfa256b34edcfdb3f42d714b40e84d7b17b9b
John Blum <jblum0@gmail.com>
2025-12-27 00:00:17 -0800

    Simplify AudioInputStreamBuilder using AudioUtils

--- shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/ext/javax/sound/sample/AudioInputStreamBuilder.java
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/ext/javax/sound/sample/AudioInputStreamBuilder.java
@@ -15,28 +15,30 @@
  */
 package io.packt.spring.ai.examples.app.shazam.ext.javax.sound.sample;
 
+import java.io.IOException;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 
-import io.codeprimate.extensions.util.ExceptionThrowingSupplier;
 import io.packt.spring.ai.examples.app.shazam.model.Audio;
 
-import org.springframework.util.Assert;
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.Builder;
 
 import lombok.AccessLevel;
 import lombok.Getter;
 
 /**
- * Builder for an {@link AudioInputStream}.
+ * Builder for {@link AudioInputStream}.
  *
  * @author John Blum
  * @see Audio
  * @see javax.sound.sampled.AudioInputStream
+ * @see org.cp.elements.lang.Builder
  * @since 0.1.0
  */
 @Getter(AccessLevel.PROTECTED)
-public class AudioInputStreamBuilder {
+public class AudioInputStreamBuilder implements Builder<AudioInputStream> {
 
 	public static AudioInputStreamBuilder from(Audio audio) {
 		return new AudioInputStreamBuilder(audio);
@@ -44,28 +46,28 @@ public class AudioInputStreamBuilder {
 
 	private final Audio audio;
 
-	AudioInputStreamBuilder(Audio audio) {
+	protected AudioInputStreamBuilder(Audio audio) {
 		Assert.notNull(audio, "Audio is required");
 		this.audio = audio;
-	}
-
-	protected AudioInputStream getAudioInputStream(Audio audio) {
-		return ExceptionThrowingSupplier.getSafely(() ->
-			AudioSystem.getAudioInputStream(audio.inputStream()));
-	}
-
-	protected AudioFormat resolveAudioFormat(Audio audio, AudioInputStream audioInputStream) {
-		return AudioFormatBuilder.from(audio)
-			.with(audioInputStream)
-			.build();
 	}
 
 	public AudioInputStream build() {
 
 		Audio audio = getAudio();
-		AudioInputStream audioInputStream = getAudioInputStream(audio);
-		AudioFormat resolvedAudioFormat = resolveAudioFormat(audio, audioInputStream);
 
-		return new AudioInputStream(audio.inputStream(), resolvedAudioFormat, audioInputStream.getFrameLength());
+		try (AudioInputStream audioInputStream = AudioUtils.openInputStream(audio)) {
+			long frameLength = audioInputStream.getFrameLength();
+			AudioFormat audioFormat = buildAudioFormat(audio, audioInputStream);
+			return new AudioInputStream(audio.inputStream(), audioFormat, frameLength);
+		}
+		catch (IOException cause) {
+			throw new RuntimeException("Failed to open InputStream to Audio", cause);
+		}
+	}
+
+	protected AudioFormat buildAudioFormat(Audio audio, AudioInputStream audioInputStream) {
+		return AudioFormatBuilder.from(audio)
+			.copyFormatFrom(audioInputStream)
+			.build();
 	}
 }
