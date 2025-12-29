cdfbaf1445bb893db633016cf00bec4c8542af6a
John Blum <jblum0@gmail.com>
2025-12-29 14:40:04 -0800

    Create Shazam implementation of AudioFormat based on javax.sound.sampled.AudioFormat and FFProbe

---
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/ext/javax/sound/sample/ShazamAudioFormat.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.shazam.ext.javax.sound.sample;
+
+import static io.packt.spring.ai.examples.app.shazam.support.NumberUtils.BITS_PER_BYTE;
+import static io.packt.spring.ai.examples.app.shazam.support.NumberUtils.asInt;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.sound.sampled.AudioFormat;
+
+import io.packt.spring.ai.examples.app.shazam.ext.ffmpeg.FFProbe;
+import io.packt.spring.ai.examples.app.shazam.model.Audio;
+
+import org.cp.elements.lang.Assert;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+/**
+ * {@link AudioFormat} implementation for Shazam used to capture additional metadata, like duration of {@link Audio}.
+ *
+ * @author John Blum
+ * @see Audio
+ * @see FFProbe
+ * @see javax.sound.sampled.AudioFormat
+ * @since 0.1.0
+ */
+@SuppressWarnings("unused")
+@Getter(AccessLevel.PROTECTED)
+public class ShazamAudioFormat extends AudioFormat {
+
+	private final AtomicReference<FFProbe.Format> probeFormat = new AtomicReference<>();
+	private final AtomicReference<Integer> sampleSizeInBits = new AtomicReference<>();
+
+	private final Audio audio;
+
+	public ShazamAudioFormat(Audio audio, Encoding encoding, float sampleRate, int sampleSizeInBits, int channels,
+			float frameRate, int frameSize, boolean bigEndian, Map<String, Object> properties) {
+
+		super(encoding, sampleRate, sampleSizeInBits, channels, frameSize, frameRate, bigEndian, properties);
+		Assert.notNull(audio, "Audio is required");
+		this.audio = audio;
+	}
+
+	public int getAudioSize() {
+		return getAudio().getData().length;
+	}
+
+	public Integer getBitRate() {
+		return getProbeFormat().bitRate();
+	}
+
+	public Duration getDuration() {
+		return getProbeFormat().getDuration();
+	}
+
+	public FFProbe.Format getProbeFormat() {
+		return this.probeFormat.updateAndGet(format -> format != null ? format
+			: AudioUtils.probeFormat(getAudio()));
+	}
+
+	@Override
+	public int getSampleSizeInBits() {
+		return this.sampleSizeInBits.updateAndGet(this::resolveSampleSizeInBits);
+	}
+
+	private Integer resolveSampleSizeInBits(Integer sampleSizeInBits) {
+
+		if (sampleSizeInBits == null) {
+
+			sampleSizeInBits = super.getSampleSizeInBits();
+
+			if (AudioUtils.isNotSpecified(sampleSizeInBits)) {
+
+				int audioSizeInBytes = getSize();
+				int audioSizeInBits = audioSizeInBytes * BITS_PER_BYTE;
+				int audioDurationInSeconds = asInt(getDuration().toSeconds());
+				int sampleRate = asInt(getSampleRate()); // samples per second
+				int totalSamples = audioDurationInSeconds * sampleRate;
+
+				sampleSizeInBits = audioSizeInBits / totalSamples;
+			}
+
+		}
+
+		return sampleSizeInBits;
+	}
+
+	public Integer getSize() {
+		int size = getAudioSize();
+		return AudioUtils.isSpecified(size) ? size
+			: getProbeFormat().size();
+	}
+}
