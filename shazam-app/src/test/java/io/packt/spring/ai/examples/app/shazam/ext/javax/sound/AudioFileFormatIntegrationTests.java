4f9e0fb6ad7c2b34ed92092f9178770abd5a19e7
John Blum <jblum0@gmail.com>
2025-12-29 17:26:24 -0800

    Add Integration Tests for AudioFileFormat

---
+++ shazam-app/src/test/java/io/packt/spring/ai/examples/app/shazam/ext/javax/sound/AudioFileFormatIntegrationTests.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.packt.spring.ai.examples.app.shazam.ext.javax.sound;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
+
+import io.packt.spring.ai.examples.app.shazam.ext.javax.sound.sample.AudioUtils;
+import io.packt.spring.ai.examples.app.shazam.model.Audio;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * Integration Tests for Java Sound {@link AudioFileFormat}.
+ *
+ * @author John Blum
+ * @see Audio
+ * @see javax.sound.sampled.AudioFileFormat
+ * @see org.junit.jupiter.api.Test
+ * @since 0.1.0
+ */
+class AudioFileFormatIntegrationTests {
+
+	private static final String RESOURCE_PATH = "Matchbox20-Unwell.mp3";
+
+	@Test
+	@EnabledIf("resourceExists")
+	void audioFileFormat() {
+
+		Audio audio = Audio.from(resource());
+
+		assertThat(audio).isNotNull();
+		assertThat(audio.getData()).isNotEmpty();
+
+		AudioFileFormat audioFileFormat = AudioUtils.resolveAudioFileFormat(audio);
+
+		assertThat(audioFileFormat).isNotNull();
+		assertThat(audioFileFormat.getByteLength()).isEqualTo(audio.getData().length);
+		assertThat(audioFileFormat.getFrameLength()).isEqualTo(AudioSystem.NOT_SPECIFIED);
+		assertThat(audioFileFormat.getType().toString().toLowerCase()).isEqualTo("mpeg");
+		assertThat(audioFileFormat.properties()).isNull();
+	}
+
+	Resource resource() {
+		return new ClassPathResource(RESOURCE_PATH);
+	}
+
+	boolean resourceExists() {
+		return resource().exists();
+	}
+}
