85269e70030365a3a860c00cb00628e313c31d54
John Blum <jblum0@gmail.com>
2026-02-01 15:48:18 -0800

    Test WAV audio data in FFProbeIntegrationTests
    In summary, the test class tests:
    
    1. MP3 from Internet
    2. MP3 from Album
    3. WAV from Album

--- shazam-app/src/test/java/io/packt/spring/ai/examples/app/shazam/ext/ffmpeg/FFProbeIntegrationTests.java
+++ shazam-app/src/test/java/io/packt/spring/ai/examples/app/shazam/ext/ffmpeg/FFProbeIntegrationTests.java
@@ -19,12 +19,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Duration;
 
+import io.packt.spring.ai.examples.app.shazam.AbstractShazamIntegrationTests;
 import io.packt.spring.ai.examples.app.shazam.model.Audio;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIf;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 /**
@@ -32,13 +32,15 @@ import org.springframework.core.io.Resource;
  *
  * @author John Blum
  * @see FFProbe
+ * @see AbstractShazamIntegrationTests
  * @see org.junit.jupiter.api.Test
  * @since 0.1.0
  */
-public class FFProbeIntegrationTests {
+class FFProbeIntegrationTests extends AbstractShazamIntegrationTests {
 
-	private static final String MATCHBOX20_UNWELL_RESOURCE_PATH = "Matchbox20-Unwell.mp3";
-	private static final String PEARL_JAM_NO_CODE_RED_MOSQUITO_RESOURCE_PATH = "PearlJam-NoCode-RedMosquito.mp3";
+	private static final String MATCHBOX20_UNWELL_RESOURCE_PATH = "Matchbox20-Unwell.mp3"; // Internet
+	private static final String PEARL_JAM_NO_CODE_RED_MOSQUITO_RESOURCE_PATH = "PearlJam-NoCode-RedMosquito.mp3"; // Album
+	private static final String PEARL_JAM_TEN_JEREMY_RESOURCE_PATH = "PearlJam-Ten-Jeremy.wav"; // Album
 
 	@Test
 	@EnabledIf("matchbox20UnwellResourceExists")
@@ -82,7 +84,7 @@ public class FFProbeIntegrationTests {
 		assertThat(format.bitRate()).isGreaterThanOrEqualTo(160_000);
 		assertThat(format.getDuration()).isGreaterThan(Duration.ofMinutes(4).plusSeconds(3));
 		assertThat(format.filename()).isEqualTo(audio.file().getAbsolutePath());
-		assertThat(format.name().toLowerCase()).isEqualTo("mp3");
+		assertThat(format.name().toLowerCase()).isEqualToIgnoringCase("MP3");
 		assertThat(format.description()).contains("MPEG audio layer 2/3");
 		assertThat(format.numberOfPrograms()).isZero();
 		assertThat(format.numberOfStreams()).isOne();
@@ -92,8 +94,39 @@ public class FFProbeIntegrationTests {
 		assertThat(format.startTime()).isEqualTo(0.0d);
 	}
 
+	@Test
+	@EnabledIf("pearlJamTenJeremyResourceExists")
+	void pearlJamTenJeremyAudioFormatIsCorrect() {
+
+		Audio audio = Audio.from(pearlJamTenJeremyResource());
+
+		FFProbe probe = FFProbe.builder()
+			.enableDownload()
+			.build();
+
+		FFProbe.Format format = probe.showFormat(audio);
+
+		assertThat(format).isNotNull();
+		assertThat(format.bitRate()).isGreaterThanOrEqualTo(160_000);
+		assertThat(format.getDuration()).isGreaterThan(Duration.ofMinutes(5).plusSeconds(18));
+		assertThat(format.filename()).isEqualTo(audio.file().getAbsolutePath());
+		assertThat(format.name().toLowerCase()).isEqualToIgnoringCase("WAV");
+		assertThat(format.description()).contains("WAV / WAVE (Waveform Audio)");
+		assertThat(format.numberOfPrograms()).isZero();
+		assertThat(format.numberOfStreams()).isOne();
+		assertThat(format.numberOfStreamGroups()).isZero();
+		assertThat(format.probeScore()).isEqualTo(99); // TODO: ?
+		assertThat(format.size()).isGreaterThan(56_300_000);
+ 		assertThat(format.startTime()).isNull(); // TODO: ?
+	}
+
+	@Override
+	protected String resourcePath() {
+		return MATCHBOX20_UNWELL_RESOURCE_PATH;
+	}
+
 	Resource matchbox20UnwellResource() {
-		return new ClassPathResource(MATCHBOX20_UNWELL_RESOURCE_PATH);
+		return resource(MATCHBOX20_UNWELL_RESOURCE_PATH);
 	}
 
 	boolean matchbox20UnwellResourceExists() {
@@ -101,10 +134,18 @@ public class FFProbeIntegrationTests {
 	}
 
 	Resource pearlJamNoCodeRedMosquitoResource() {
-		return new ClassPathResource(PEARL_JAM_NO_CODE_RED_MOSQUITO_RESOURCE_PATH);
+		return resource(PEARL_JAM_NO_CODE_RED_MOSQUITO_RESOURCE_PATH);
 	}
 
 	boolean pearlJamNoCodeRedMosquitoResourceExists() {
 		return pearlJamNoCodeRedMosquitoResource().exists();
+	}
+
+	Resource pearlJamTenJeremyResource() {
+		return resource(PEARL_JAM_TEN_JEREMY_RESOURCE_PATH);
+	}
+
+	boolean pearlJamTenJeremyResourceExists() {
+		return pearlJamTenJeremyResource().exists();
 	}
 }
