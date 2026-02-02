bd7483a8dfcc772041fa3a72a3dfbd15d1d5ac3e
John Blum <jblum0@gmail.com>
2026-02-01 16:26:39 -0800

    Test WAV audio data in AudioFileFormatIntegrationTests
    Extens AbstractShazamIntegrationTests

--- shazam-app/src/test/java/io/packt/spring/ai/examples/app/shazam/ext/javax/sound/AudioFileFormatIntegrationTests.java
+++ shazam-app/src/test/java/io/packt/spring/ai/examples/app/shazam/ext/javax/sound/AudioFileFormatIntegrationTests.java
@@ -20,15 +20,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 
 import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import io.packt.spring.ai.examples.app.shazam.AbstractShazamIntegrationTests;
 import io.packt.spring.ai.examples.app.shazam.model.Audio;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIf;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 /**
@@ -36,23 +37,27 @@ import org.springframework.core.io.Resource;
  *
  * @author John Blum
  * @see Audio
+ * @see AbstractShazamIntegrationTests
  * @see javax.sound.sampled.AudioFileFormat
  * @see org.junit.jupiter.api.Test
  * @since 0.1.0
  */
 @SuppressWarnings("unused")
-class AudioFileFormatIntegrationTests {
+class AudioFileFormatIntegrationTests extends AbstractShazamIntegrationTests {
 
-	private static final String MATCHBOX20_UNWELL_RESOURCE_PATH = "Matchbox20-Unwell.mp3";
+	private static final String MATCHBOX20_UNWELL_RESOURCE_PATH = "Matchbox20-Unwell.mp3"; // Internet
 
 	// Throws UnsupportedAudioFileException: File of unsupported format
-	private static final String PEARL_JAM_NO_CODE_RED_MOSQUITO_RESOURCE_PATH = "PearlJam-NoCode-RedMosquito.mp3";
+	private static final String PEARL_JAM_NO_CODE_RED_MOSQUITO_RESOURCE_PATH = "PearlJam-NoCode-RedMosquito.mp3"; // Album
 
+	private static final String PEARL_JAM_TEN_JEREMY_RESOURCE_PATH = "PearlJam-Ten-Jeremy.wav";
+
+	//private static final String RESOURCE_PATH = PEARL_JAM_NO_CODE_RED_MOSQUITO_RESOURCE_PATH;
 	private static final String RESOURCE_PATH = MATCHBOX20_UNWELL_RESOURCE_PATH;
 
 	@Test
 	@EnabledIf("resourceExists")
-	void audioFileFormat() throws UnsupportedAudioFileException, IOException {
+	void mp3AudioFileFormat() throws UnsupportedAudioFileException, IOException {
 
 		Audio audio = Audio.from(resource());
 		AudioFileFormat audioFileFormat = AudioSystem.getAudioFileFormat(audio.file());
@@ -62,13 +67,39 @@ class AudioFileFormatIntegrationTests {
 		assertThat(audioFileFormat.getFrameLength()).isEqualTo(AudioSystem.NOT_SPECIFIED);
 		assertThat(audioFileFormat.getType().getExtension()).isEqualTo("mpeg");
 		assertThat(audioFileFormat.properties()).isNull();
+		assertAudioFormat(audioFileFormat.getFormat(), new AudioFormat.Encoding("MPEG2L3"));
 	}
 
-	Resource resource() {
-		return new ClassPathResource(RESOURCE_PATH);
+	@Test
+	@EnabledIf("wavResourceExists")
+	void wavAudioFileFormat() throws UnsupportedAudioFileException, IOException {
+
+		Audio audio = Audio.from(wavResource());
+		AudioFileFormat audioFileFormat = AudioSystem.getAudioFileFormat(audio.file());
+
+		assertThat(audioFileFormat).isNotNull();
+		assertThat(audioFileFormat.getByteLength()).isEqualTo(audio.getData().length);
+		assertThat(audioFileFormat.getFrameLength()).isGreaterThan(0); // TODO: ?
+		assertThat(audioFileFormat.getType()).isEqualTo(AudioFileFormat.Type.WAVE);
+		assertThat(audioFileFormat.properties()).isEmpty();
+		assertAudioFormat(audioFileFormat.getFormat(), AudioFormat.Encoding.PCM_SIGNED);
 	}
 
-	boolean resourceExists() {
-		return resource().exists();
+	void assertAudioFormat(AudioFormat audioFormat, AudioFormat.Encoding encoding) {
+		assertThat(audioFormat).isNotNull();
+		assertThat(audioFormat.getEncoding()).isEqualTo(encoding);
+	}
+
+	@Override
+	protected String resourcePath() {
+		return RESOURCE_PATH;
+	}
+
+	Resource wavResource() {
+		return resource(PEARL_JAM_TEN_JEREMY_RESOURCE_PATH);
+	}
+
+	boolean wavResourceExists() {
+		return wavResource().exists();
 	}
 }
