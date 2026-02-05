525d8f2ed9bad50fd0b498ef4c3d518c55c04bde
John Blum <jblum0@gmail.com>
2026-02-04 20:26:20 -0800

    Refactor and simplify AudioFormatResolverApp

--- shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/support/AudioFormatResolverApp.java
+++ shazam-app/src/main/java/io/packt/spring/ai/examples/app/shazam/support/AudioFormatResolverApp.java
@@ -15,18 +15,9 @@
  */
 package io.packt.spring.ai.examples.app.shazam.support;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
-
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
 
-import io.codeprimate.extensions.util.ExceptionThrowingRunnable;
-import io.codeprimate.extensions.util.ExceptionThrowingSupplier;
 import io.packt.spring.ai.examples.app.shazam.ext.javax.sound.sample.AudioFormatBuilder;
-import io.packt.spring.ai.examples.app.shazam.ext.javax.sound.sample.AudioFormatResolver;
-import io.packt.spring.ai.examples.app.shazam.ext.javax.sound.sample.AudioInputStreamBuilder;
 import io.packt.spring.ai.examples.app.shazam.ext.javax.sound.sample.ShazamAudioFormat;
 import io.packt.spring.ai.examples.app.shazam.model.Audio;
 
@@ -35,20 +26,21 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Java program used to resolve (introspect) the {@link AudioFormat} of an {@link Audio} {@link Resource}.
  *
  * @author John Blum
  * @see Audio
  * @see AudioFormatBuilder
+ * @see ShazamAudioFormat
  * @see java.lang.Runnable
  * @see javax.sound.sampled.AudioFormat
- * @see org.springframework.core.io.Resource
  * @since 0.1.0
  */
+@Slf4j(topic = "shazam-app")
 public class AudioFormatResolverApp implements Runnable {
-
-	private static final boolean DEBUG = false;
 
 	private static final String USAGE_MESSAGE = "$ java -cp <classpath> %s </path/to/audio/file[.mp3]>%n";
 
@@ -60,7 +52,7 @@ public class AudioFormatResolverApp implements Runnable {
 
 		if (args.length < 1) {
 			String message = USAGE_MESSAGE.formatted(AudioFormatResolverApp.class.getName());
-			System.err.printf(message);
+			log.error(message);
 			System.exit(-1);
 		}
 
@@ -82,73 +74,30 @@ public class AudioFormatResolverApp implements Runnable {
 
 		String resourcePath = getFirstArgument();
 		Resource resource = newResource(resourcePath);
-
-		if (!resource.exists()) {
-			printError("Audio resource [%s] not found%n", resource);
-			System.exit(-2);
-		}
-
 		Audio audio = newAudio(resource);
+		AudioFormat audioFormat = AudioFormatBuilder.from(audio).build();
 
-		ExceptionThrowingRunnable.runSafely(() -> {
-			try (AudioInputStream audioInputStream = openInputStream(audio)) {
-				AudioFormat audioFormat = audioInputStream.getFormat();
-				printOut("Audio resource [%s]", resource);
-				printOut("Audio format [%s]%n", audioFormat);
-				printOut("Audio duration [%s]%n", ((ShazamAudioFormat) audioFormat).getDuration());
-			}
-			catch (IOException cause) {
-				printError("Failed to determine the format of audio [%s] because: %s%n", resource, cause.getMessage());
-				debug(() -> printError(cause));
-				System.exit(-3);
-			}
-		});
+		log.info("**AudioFormat Resolver Application**");
+		log.info("Audio resource [{}]", resource);
+		log.info("Audio format [{}]", audioFormat);
+		log.info("Audio duration [{}]", ((ShazamAudioFormat) audioFormat).getDuration());
 	}
 
 	private Audio newAudio(Resource resource) {
 		Assert.notNull(resource, "Resource is required");
-		Assert.isTrue(resource.isFile(), "Expecting resource [%s] to originate from file", resource);
-		File file = ExceptionThrowingSupplier.getSafely(resource::getFile);
-		Audio audio = Audio.from(file);
-		audio = audio.in(resolveAudioFormat(audio));
-		return audio;
+		return Audio.from(resource);
 	}
 
 	private Resource newResource(String resourcePath) {
 		Assert.hasText(resourcePath, "Resource path [%s] is required", resourcePath);
 		Resource resource = new ClassPathResource(resourcePath);
-		resource = resource.exists() ? resource : new FileSystemResource(resourcePath);
+		return resource.exists() ? resource : newFileResource(resourcePath);
+	}
+
+	private Resource newFileResource(String resourcePath) {
+		Assert.hasText(resourcePath, "Resource path [%s] is required", resourcePath);
+		Resource resource = new FileSystemResource(resourcePath);
+		Assert.isTrue(resource.exists(), "Resource path [%s] not found", resourcePath);
 		return resource;
-	}
-
-	private AudioInputStream openInputStream(Audio audio) {
-		return AudioInputStreamBuilder.from(audio).build();
-	}
-
-	private AudioFormat resolveAudioFormat(Audio audio) {
-		return AudioFormatResolver.defaultAudioFormatResolver().resolve(audio);
-	}
-
-	private void print(PrintStream out, String message, Object... arguments) {
-		out.printf(message, arguments);
-		out.flush();
-	}
-
-	private void printOut(String message, Object... arguments) {
-		print(System.out, message, arguments);
-	}
-
-	private void printError(String message, Object... arguments) {
-		print(System.err, message, arguments);
-	}
-
-	private void printError(Throwable error) {
-		error.printStackTrace(System.err);
-	}
-
-	private void debug(Runnable action) {
-		if (DEBUG) {
-			action.run();
-		}
 	}
 }
