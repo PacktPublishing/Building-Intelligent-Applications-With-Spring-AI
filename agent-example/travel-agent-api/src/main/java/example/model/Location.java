ccb819bf447c6f104a107c323d9fa29ac6c51665
John Blum <jblum0@gmail.com>
2026-04-19 20:23:35 -0700

    Create Location ADT modeling a origin and destination of a flight
    The Location represent a city and country.

---
+++ agent-example/travel-agent-api/src/main/java/example/model/Location.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.model;
+
+import java.util.Locale;
+
+/**
+ * Abstract Data Type (ADT) and Java record modeling a location as city and country.
+ *
+ * @author John Blum
+ * @param cityName {@link String name} of the city, e.g. {@literal Portland, OR}.
+ * @param locale {@link Locale} of this location.
+ * @see Locale
+ * @since 0.1.0
+ */
+@SuppressWarnings("unused")
+public record Location(String cityName, Locale locale) {
+
+	public String getCountry() {
+		return locale().getCountry();
+	}
+}
