82821349588f1c68a029d68d12a5afdfb6f3ea5b
John Blum <jblum0@gmail.com>
2026-04-26 15:06:09 -0700

    Create FlightReservation ADT modeling a confirmed reservation for flight

---
+++ agent-example/travel-agent-api/src/main/java/com/packt/spring/ai/examples/agent/model/FlightReservation.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.packt.spring.ai.examples.agent.model;
+
+/**
+ * Abstract Data Type (ADT) and Java Record modeling a flight reservation.
+ *
+ * @author John Blum
+ * @param flight {@link Flight} to reserve.
+ * @param confirmationNumber {@link String confirmation number} for the reservation.
+ * @see Confirmation
+ * @see Flight
+ * @since 0.1.0
+ */
+public record FlightReservation(Flight flight, String confirmationNumber) implements Confirmation<String> {
+
+	@Override
+	public String getNumber() {
+		return confirmationNumber();
+	}
+}
