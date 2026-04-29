fee541fd786275e68fe3c0908ecf4c443e673f6b
John Blum <jblum0@gmail.com>
2026-04-29 13:46:23 -0700

    Set version to 0.2.0-SNAPSHOT

--- agent-example/bin/run-travel-agent.sh
+++ agent-example/bin/run-travel-agent.sh
@@ -4,4 +4,4 @@ java\
   -Dspring.main.web-application-type=none \
   -Dspring.ai.mcp.server.stdio=true \
   -DUSER_HOME=$HOME \
-  -jar $HOME/travel-agent-0.1.0-SNAPSHOT.jar
+  -jar $HOME/travel-agent-0.2.0-SNAPSHOT.jar
--- bin/copyTravelAgentToUserHome.sh
+++ bin/copyTravelAgentToUserHome.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp ./agent-example/travel-agent/target/travel-agent-0.1.0-SNAPSHOT.jar ~/
+cp ./agent-example/travel-agent/target/travel-agent-0.2.0-SNAPSHOT.jar ~/
