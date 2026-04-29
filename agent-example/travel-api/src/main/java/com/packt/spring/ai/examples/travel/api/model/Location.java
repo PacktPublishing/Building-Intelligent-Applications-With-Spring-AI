52b808d846eaad75d4006bae3c06373ad6031963
John Blum <jblum0@gmail.com>
2026-04-28 17:56:08 -0700

    Add from(:GpsCoordinates) factory method to Location

--- agent-example/travel-api/src/main/java/com/packt/spring/ai/examples/travel/api/model/Location.java
+++ agent-example/travel-api/src/main/java/com/packt/spring/ai/examples/travel/api/model/Location.java
@@ -17,20 +17,29 @@ package com.packt.spring.ai.examples.travel.api.model;
 
 import java.util.Locale;
 
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.Constants;
+
 /**
  * Abstract Data Type (ADT) modeling a location as city, area and country, with {@link GpsCoordinates}.
  *
  * @author John Blum
  * @param city {@link String name} of the city, e.g. {@literal Portland, OR}.
  * @param area {@link String name} of the area, such as the state of {@literal California (CA)} in the {@literal USA}
- * or the the province of {@literal Ontario} in {@literal Canada}.
+ * or the province of {@literal Ontario} in {@literal Canada}.
  * @param locale {@link Locale} of this location.
+ * @param coordinates {@link GpsCoordinates} of this location.
  * @see GpsCoordinates
  * @see Locale
  * @since 0.1.0
  */
 @SuppressWarnings("unused")
 public record Location(String city, String area, Locale locale, GpsCoordinates coordinates) {
+
+	public static Location from(GpsCoordinates coordinates) {
+		Assert.notNull(coordinates, "GPS coordinates are required");
+		return new Location(Constants.UNKNOWN, Constants.UNKNOWN, Locale.getDefault(), coordinates);
+	}
 
 	public String getCountry() {
 		return locale().getCountry();
