e520d721d736c37507bb9f577c0b0ad99361dda0
John Blum <jblum0@gmail.com>
2026-04-28 19:28:43 -0700

    Create VehicleSearchRequest ADT modeling a search request for a vehicle rental

---
+++ agent-example/travel-api/src/main/java/com/packt/spring/ai/examples/travel/api/model/VehicleSearchRequest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2024 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.packt.spring.ai.examples.travel.api.model;
+
+/**
+ * Abstract Data Type (ADT) modeling a search request for a {@link VehicleRental}.
+ *
+ * @author John Blum
+ * @see VehicleRental
+ * @since 0.1.0
+ */
+public class VehicleSearchRequest {
+
+}
