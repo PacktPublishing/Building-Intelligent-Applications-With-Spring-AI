e961896496ce8cb6ef1d8254148a2fa385b61912
John Blum <jblum0@gmail.com>
2026-05-03 13:58:44 -0700

    Add Javadoc to TravelService

--- agent-example/travel-api/src/main/java/com/packt/spring/ai/examples/travel/api/service/TravelService.java
+++ agent-example/travel-api/src/main/java/com/packt/spring/ai/examples/travel/api/service/TravelService.java
@@ -15,10 +15,13 @@
  */
 package com.packt.spring.ai.examples.travel.api.service;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
+import com.packt.spring.ai.examples.travel.api.model.Airport;
 import com.packt.spring.ai.examples.travel.api.model.Flight;
 import com.packt.spring.ai.examples.travel.api.model.FlightSearchRequest;
+import com.packt.spring.ai.examples.travel.api.model.Hotel;
 import com.packt.spring.ai.examples.travel.api.model.HotelBooking;
 import com.packt.spring.ai.examples.travel.api.model.HotelSearchRequest;
 import com.packt.spring.ai.examples.travel.api.model.VehicleRental;
@@ -36,10 +39,39 @@ import com.packt.spring.ai.examples.travel.api.model.VehicleSearchRequest;
 @SuppressWarnings("unused")
 public interface TravelService {
 
+	/**
+	 * Searches for available {@link Flight Flights} departing from {@link Airport} at a specified {@link ZonedDateTime}
+	 * and arriving at a designated {@link Airport}.
+	 *
+	 * @param request {@link FlightSearchRequest} defining the search criteria used to match {@link Flight Flights}.
+	 * @return a {@link List} of all matching {@link Flight Flights}.
+	 * @see FlightSearchRequest
+	 * @see Flight
+	 * @see List
+	 */
 	List<Flight> searchFlights(FlightSearchRequest request);
 
+	/**
+	 * Find {@link Hotel Hotels} with vacancy matching the given {@link HotelSearchRequest}.
+	 *
+	 * @param request {@link HotelSearchRequest} defining search criteria used to match available {@link Hotel Hotels}.
+	 * @return a {@link List} of {@link HotelBooking HotelBookings} available.
+	 * @see HotelSearchRequest
+	 * @see HotelBooking
+	 * @see List
+	 */
 	List<HotelBooking> findHotels(HotelSearchRequest request);
 
+	/**
+	 * Search for available {@link VehicleRental VehicleRentals} matching the given {@link VehicleSearchRequest}.
+	 *
+	 * @param request {@link VehicleSearchRequest} defining search criteria used to match a {@link VehicleRental}.
+	 * @return a {@link List} of matching {@link VehicleRental VehicleRentals}.
+	 * @throws UnsupportedOperationException by default.
+	 * @see VehicleSearchRequest
+	 * @see VehicleRental
+	 * @see List
+	 */
 	default List<VehicleRental> rentVehicle(VehicleSearchRequest request) {
 		throw new UnsupportedOperationException("Vehicle rentals not supported");
 	}
