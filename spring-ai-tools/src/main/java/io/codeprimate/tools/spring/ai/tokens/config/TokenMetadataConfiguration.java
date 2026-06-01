893e914b5adacac2362fc5d59ca1c063cc841b4e
John Blum <jblum0@gmail.com>
2026-06-01 15:23:13 -0700

    Rename TokenMetadataApplicationConfiguration to TokenMetadataConfiguration

--- spring-ai-tools/src/main/java/io/codeprimate/tools/spring/ai/tokens/config/TokenMetadataConfiguration.java
+++ spring-ai-tools/src/main/java/io/codeprimate/tools/spring/ai/tokens/config/TokenMetadataConfiguration.java
@@ -34,7 +34,7 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootConfiguration
 @SuppressWarnings("unused")
-public class TokenMetadataApplicationConfiguration {
+public class TokenMetadataConfiguration {
 
 	@Bean
 	public TokenCountEstimator tokenCountEstimator(
